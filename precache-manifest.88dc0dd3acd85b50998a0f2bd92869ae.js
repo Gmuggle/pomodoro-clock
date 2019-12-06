b09b37356ce76fa18299b6dc52b04c6081499a27
Gmuggle <you@example.com>
2019-12-06 14:32:56 +0800

    Updates

---
+++ precache-manifest.88dc0dd3acd85b50998a0f2bd92869ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d15847728f01c88e5ce68ffffd5475",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "9c390c1a89157236653b",
+    "url": "/pomodoro-clock/static/css/2.7f4559f2.chunk.css"
+  },
+  {
+    "revision": "0d8bf89c5ed5ecbf6285",
+    "url": "/pomodoro-clock/static/css/main.4c2205bf.chunk.css"
+  },
+  {
+    "revision": "9c390c1a89157236653b",
+    "url": "/pomodoro-clock/static/js/2.28a153db.chunk.js"
+  },
+  {
+    "revision": "0d8bf89c5ed5ecbf6285",
+    "url": "/pomodoro-clock/static/js/main.bf4af19b.chunk.js"
+  },
+  {
+    "revision": "e1ed92054e9156825b1e",
+    "url": "/pomodoro-clock/static/js/runtime-main.662da264.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/pomodoro-clock/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/pomodoro-clock/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/pomodoro-clock/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/pomodoro-clock/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/pomodoro-clock/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "cf48bcdb51b06e08119fba19df24e47e",
+    "url": "/pomodoro-clock/static/media/yisell_sound_2008030716560441118_88011.cf48bcdb.mp3"
+  }
+]);
